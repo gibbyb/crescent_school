8ad1c1c7e6425014588e11f121e70df45541c78f
gibbyb <gib@gibbyb.com>
2024-05-21 11:50:37 -0500

    Init commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        Hello World!
+      </div>
+    </main>
+  );
+}
