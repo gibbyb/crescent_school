6d6c2bb548900e28aa914577c2b75a9cf391c241
gibbyb <gib@gibbyb.com>
2024-05-21 14:53:35 -0500

    Set up database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
+import { db } from "~/server/db"
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         Hello World!
       </div>
     </main>
